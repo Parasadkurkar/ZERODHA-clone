f9a3da46268a49a264638ecc0141fe14e77c5f99
paras <parasadkurkar37@gmail.com>
2025-10-07 00:24:36 +0530

    Updated About Page

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav
@@ -6,13 +7,13 @@ function Navbar() {
       style={{ backgroundColor: "#FFF" }}
     >
       <div class="container">
-        <a class="navbar-brand" href="#">
+        <Link class="navbar-brand" to="/">
           <img
             src="media/images/logo.svg"
             style={{ width: "25%" }}
             alt="zerodhalogo"
           />
-        </a>
+        </Link>
         <button
           class="navbar-toggler"
           type="button"
@@ -27,14 +28,14 @@ function Navbar() {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav  mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
+              <Link class="nav-link active" aria-current="page" to="signup">
                 Signup
-              </a>
+              </Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
+              <Link class="nav-link active" aria-current="page" to="about">
                 About
-              </a>
+              </Link>
             </li>
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">
---
+++ src/landing_page/signup/Signup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+function Signup() {
+    return (
+        <h1>Paras</h1>
+      );
+}
+
+export default Signup;
+<h1>Paras</h1>
